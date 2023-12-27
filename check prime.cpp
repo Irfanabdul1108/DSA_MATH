f912e88a7a8d796e63c7c61053642c9420086b3e
IndianIrfan <143101267+IndianIrfan@users.noreply.github.com>
2023-12-27 19:20:55 +0530

    Add files via upload

---
+++ check prime.cpp
@@ -0,0 +1,12 @@
+// platform - coding ninjas
+//link - https://www.codingninjas.com/studio/problems/check-prime_624934?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+// concept - check prime
+bool isPrime(int n)
+{
+  for (int i = 1; i <= n; i++) {
+  if(n%i==0)
+    count+=1;
+  }
+if(count==2;
+  return count!=2;
+}
